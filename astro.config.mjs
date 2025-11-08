16569398fc270e60097ebbd6d16e5426ec6d6397
fmoreira99 <franciscomoreira1599@gmail.com>
2025-11-08 11:04:31 -0500

    implementacion de estilos
    implementacion de estilos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://fmoreira99.github.io/runamedios/',
+  base: '/runamedios/',
   output: 'static',
-  outDir: 'docs',
 });
