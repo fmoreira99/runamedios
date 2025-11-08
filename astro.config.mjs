1755eb6ba2a1a02c35e66c1878a3240cc5c7f234
fmoreira99 <franciscomoreira1599@gmail.com>
2025-11-08 12:03:23 -0500

    arreglo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ export default defineConfig({
   site: 'https://fmoreira99.github.io', // SOLO el dominio
   base: '/runamedios',                  // SIN / al final
   output: 'static',
-  build: {
-    outDir: 'docs',                     // Astro construye directo en /docs
-  },
+  outDir: 'docs',                       // Astro construye directo en /docs
 });
