ef93adc7b81bf6811b629c83090e4f7a811bbd73
fmoreira99 <franciscomoreira1599@gmail.com>
2025-11-08 12:39:19 -0500

    informacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://fmoreira99.github.io', // SOLO el dominio
+  site: 'https://noticias.zonasilau.site', // SOLO el dominio
   base: '/runamedios',                  // SIN / al final
   output: 'static',
   outDir: 'docs',                       // Astro construye directo en /docs
