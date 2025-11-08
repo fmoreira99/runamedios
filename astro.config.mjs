13b749e5551347d75780e0803b84679278390145
fmoreira99 <franciscomoreira1599@gmail.com>
2025-11-08 11:22:27 -0500

    actual

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://fmoreira99.github.io/runamedios/',
+  site: 'https://fmoreira99.github.io/runamedios',
   base: '/runamedios/',
   output: 'static',
+  build: {
+    assets: '_astro', // asegúrate de usar esta carpeta
+  },
 });
